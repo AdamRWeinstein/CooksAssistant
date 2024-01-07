9d365ff6f510b0fb11f3674c5fbff02ef7c90cbe
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2024-01-07 13:34:05 -0500

    Add remove ingredient and remove step functionality, add CSS

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -15,21 +15,33 @@ document.getElementById('addStepButton').addEventListener('click', () => {
 
     // Update the ID of the new step and its child elements
     newStep.id = 'step' + stepCount;
+    const header = newStep.querySelector('h1');
+    header.textContent = "Step " + stepCount;
     const instructions = newStep.querySelector('textarea');
     instructions.id = 'recipeStep' + stepCount;
+    instructions.classList.add('recipeStep');
     instructions.placeholder = 'Step ' + stepCount;
     instructions.value = '';
     const addIngredientButton = newStep.querySelector('.ingredients button');
+    addIngredientButton.classList.add('addIngredientButton');
     addIngredientButton.id = 'addIngredientButton' + stepCount;
     addIngredientButton.addEventListener('click', handleAddIngredientClick)
 
-    newStep.querySelector('.ingredients div').id = 'ingredientsContainer' + stepCount;
+    const ingredientsContainer = newStep.querySelector('.ingredients div');
+    ingredientsContainer.id = 'ingredientsContainer' + stepCount;
+    ingredientsContainer.classList.add('ingredientsContainer');
     newStep.querySelector('.ingredients div').innerHTML = '';
 
     // Insert the new step into the document
     const buttonsContainer = document.getElementById('buttonsContainer');
     document.querySelector('.container.create').insertBefore(newStep, buttonsContainer);
 
+});
+document.getElementById('removeStepButton').addEventListener('click', () => {
+    const steps = document.querySelectorAll('.step');
+    const lastStep = steps[steps.length - 1];
+    lastStep.remove();
+    stepCount--;
 });
 document.getElementById('saveRecipeButton').addEventListener('click', async () => {
     // Step 1: Get all the Recipe Steps and their ingredients
@@ -141,6 +153,7 @@ function handleAddIngredientClick(event) {
     // Step 2: Create new elements
     // Create a div to wrap the elements
     const ingredientWrapper = document.createElement('div');
+    ingredientWrapper.classList.add('ingredient');
 
     // Create a text field
     const name = document.createElement('input');
@@ -164,10 +177,27 @@ function handleAddIngredientClick(event) {
         measurementUnit.appendChild(option);
     });
 
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('deleteButton');
+    deleteButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 50 59" class="bin">
+            <path fill="#B5BAC1" d="M0 7.5C0 5.01472 2.01472 3 4.5 3H45.5C47.9853 3 50 5.01472 50 7.5V7.5C50 8.32843 49.3284 9 48.5 9H1.5C0.671571 9 0 8.32843 0 7.5V7.5Z"></path>
+            <path fill="#B5BAC1" d="M17 3C17 1.34315 18.3431 0 20 0H29.3125C30.9694 0 32.3125 1.34315 32.3125 3V3H17V3Z"></path>
+            <path fill="#B5BAC1" d="M2.18565 18.0974C2.08466 15.821 3.903 13.9202 6.18172 13.9202H43.8189C46.0976 13.9202 47.916 15.821 47.815 18.0975L46.1699 55.1775C46.0751 57.3155 44.314 59.0002 42.1739 59.0002H7.8268C5.68661 59.0002 3.92559 57.3155 3.83073 55.1775L2.18565 18.0974ZM18.0003 49.5402C16.6196 49.5402 15.5003 48.4209 15.5003 47.0402V24.9602C15.5003 23.5795 16.6196 22.4602 18.0003 22.4602C19.381 22.4602 20.5003 23.5795 20.5003 24.9602V47.0402C20.5003 48.4209 19.381 49.5402 18.0003 49.5402ZM29.5003 47.0402C29.5003 48.4209 30.6196 49.5402 32.0003 49.5402C33.381 49.5402 34.5003 48.4209 34.5003 47.0402V24.9602C34.5003 23.5795 33.381 22.4602 32.0003 22.4602C30.6196 22.4602 29.5003 23.5795 29.5003 24.9602V47.0402Z" clip-rule="evenodd" fill-rule="evenodd"></path>
+            <path fill="#B5BAC1" d="M2 13H48L47.6742 21.28H2.32031L2 13Z"></path>
+        </svg>
+    `;
+
     // Step 4: Append elements to the wrapper
     ingredientWrapper.appendChild(name);
     ingredientWrapper.appendChild(quantity);
     ingredientWrapper.appendChild(measurementUnit);
+    ingredientWrapper.appendChild(deleteButton);
+
+    // Step 5: Attach an event listener to the delete button
+    deleteButton.addEventListener('click', function() {
+        ingredientsContainer.removeChild(ingredientWrapper);
+    });
 
     // Step 5: Append the wrapper to the container
     ingredientsContainer.appendChild(ingredientWrapper);
