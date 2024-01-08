60d9b756e55fd4811cdc1bac30db60148d27ba2a
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2024-01-08 08:27:38 -0500

    Redirect to login page if attempting to navigate to dashboard while not logged in

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/dashboard');
 
-router.get('/', dashboardController.showDashboard);
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
+
+
+router.get('/', ensureAuthenticated, dashboardController.showDashboard);
 
 module.exports = router;
